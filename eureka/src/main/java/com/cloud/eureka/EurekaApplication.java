255498a22f2520f82353cf940fab0af23071ae32
yjll <1392085890@qq.com>
2018-08-09 22:33:52 +0800

    first_commit

---
+++ eureka/src/main/java/com/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.cloud.eureka;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaApplication.class).web(WebApplicationType.SERVLET).run(args);
+	}
+
+}
